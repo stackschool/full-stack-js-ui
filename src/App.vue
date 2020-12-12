42a83c9006485af1bbf165b2fca1524f70ae3417
Nazreen Mohamad <nxmohamad@gmail.com>
2020-12-12 12:02:28 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>{{ appTitle }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    appTitle: "Nazreen's Portfolio"
+  })
+}
+</script>
+
+<style>
+#app {
+  padding: 20px;
+}
+</style>
