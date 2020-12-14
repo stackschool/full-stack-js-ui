e9af8786fceac3f0a7488659f8d417bae91af6ae
Nazreen Mohamad <nxmohamad@gmail.com>
2020-12-14 00:23:27 +0000

    create appheader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+      <h1>{{ title }}</h1>
+      <small>{{ subtitle }}</small>
+      <p>{{ introduction }}</p>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  props: {
+    title: String,
+    subtitle: String,
+    introduction: String,
+  }
+}
+</script>
+
+<style scoped>
+header > * {
+  display: inline-block;
+  margin-right: 20px;
+}
+</style>
