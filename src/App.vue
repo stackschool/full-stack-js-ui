48fd4970d1ae8036cf7446860ccae78f83329cc9
Nazreen Mohamad <nxmohamad@gmail.com>
2020-12-14 00:32:04 +0000

    extract state

--- src/App.vue
+++ src/App.vue
@@ -21,14 +21,9 @@
 </template>
 
 <script>
-import ProjectItem from './components/ProjectItem.vue'
-import AppHeader from './components/AppHeader.vue'
-
-const STACK_OPTIONS = {
-  VUE: 'Vue.js',
-  PHP: 'PHP',
-  NODE: 'Node.js',
-}
+import ProjectItem from './components/ProjectItem.vue';
+import AppHeader from './components/AppHeader.vue';
+import { copy, projects } from './js/state';
 
 export default {
   name: 'App',
@@ -37,31 +32,8 @@ export default {
     ProjectItem,
   },
   data: () => ({
-    copy: {
-      title: "Nazreen's Portfolio",
-      subtitle: "built using Vue.js and Node.js",
-      introduction: "Hi there, my name is Nazreen, and I'm a full stack web developer. Welcome to my portfolio."
-    },
-    projects: [
-      {
-        title: "Online Food Store",
-        image: "https://i.ibb.co/ph6x1mq/2650401.jpg",
-        description: "An online store that enables customers to buy food from resellers.",
-        techStack: [
-          STACK_OPTIONS.VUE,
-          STACK_OPTIONS.PHP
-        ]
-      },
-            {
-        title: "Shares in Bitcoin",
-        image: "https://i.ibb.co/ph6x1mq/2650401.jpg",
-        description: "A page for viewing share prices in Bitcoin.",
-        techStack: [
-          STACK_OPTIONS.VUE,
-          STACK_OPTIONS.NODE
-        ]
-      }
-    ]
+    copy,
+    projects,
   })
 }
 </script>
