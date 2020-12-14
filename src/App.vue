8e732d7717e5f6704d7a7e5c82463dae384ba9f0
Nazreen Mohamad <nxmohamad@gmail.com>
2020-12-14 00:58:15 +0000

    filter

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,24 @@
       :subtitle="copy.subtitle"
       :introduction="copy.introduction"
     />
+    <div class="filter-wrapper">
+      <p>filter by tech stack:</p>
+      <select v-model="filter.techStack">
+        <option value="Vue.js">Vue.js</option>
+        <option value="PHP">PHP</option>
+        <option value="Node.js">Node.js</option>
+      </select>
+      <span @click="clearFilter">clear filter</span>
+      <ul v-if="showResultsStatement">{{ filteredProjects.length }} projects that:
+        <li v-if="filter.techStack" class="filter-statement">Showing projects that used {{ filter.techStack }}</li>
+      </ul>
+
+    </div>
     <div class="projects-wrapper">
       <h2>My Past Projects</h2>
       <p>Here are some of the projects that I've worked on.</p>
       <project-item
-        v-for="project in projects"
+        v-for="project in filteredProjects"
         :key="project.title"
         :title="project.title"
         :image="project.image"
@@ -34,7 +47,30 @@ export default {
   data: () => ({
     copy,
     projects,
-  })
+    search: '',
+    filter: {
+      techStack: null
+    }
+  }),
+  computed: {
+    filteredProjects () {
+      if (this.filter.techStack) {
+        return this.projects.filter(project => {
+          return project.techStack.includes(this.filter.techStack)
+        })
+      } else {
+        return this.projects
+      }
+    },
+    showResultsStatement () {
+      return this.filter.techStack || this.search.length > 0
+    }
+  },
+  methods: {
+    clearFilter() {
+      this.filter.techStack = null;
+    }
+  }
 }
 </script>
 
